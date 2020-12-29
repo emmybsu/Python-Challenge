db22a3c7577b51497a87b962578d08ca910d9d46
emmybsu <emjcruzmsnrn@gmail.com>
2020-12-29 17:59:53 -0500

    added homework changes pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,51 +2,37 @@ import os
 import csv
 
 budget_data_csv = os.path.join('PyBank', 'Resources', 'budget_data.csv')
-          
-
+  
 with open(budget_data_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter= ",")
 
     next(csv_reader)
 
-    # i = 0
-    # for row in csv_reader:
-    #     print(row)
-    #     i+=1
-    #     if i == 4 : 
-    #         break
-    #print(csv_reader)
     profLoss = 0
     changeProfLoss = 0
     num_rows = 0
-    for row in csv_reader:
-        profLoss +=int(row[1])
-        
-  #      if num_rows == 4 : 
- #          break
-        
-        if num_rows != 0:
-           changeProfLoss += int(row[1]) - prevProf
-
-        prevProf = int(row[1])
-
-        num_rows +=1
-
+  
+    change_Prof_Loss =[]
+    list1 = []
+    list2 = []
+    all_months = []
     
-   
-    print (num_rows)
+    for row in csv_reader:
+        list1.append(int(row[1]))
+        all_months.append(row[0])
+    
 
+    list2 = [int(list1[i]-list1[i-1]) for i in range(1,len(list1))]
+    max_month = list2.index(max(list2))
+    min_month = list2.index(min(list2))
+    The_month = all_months[max_month+1]
+    The_month2 = all_months[min_month+1]
+    
+    print('----------------------------')
     print('Financial Analysis')
     print('----------------------------')
-    print (f'Total Months: {num_rows} ')
-
-    # Count of Total Votes cast
- #  num_rows = 0
- # for row in csv_reader:
- #    num_rows +=1
-    
-    print(row)
-    print(profLoss)
-    print(profLoss/ num_rows)
-    print(changeProfLoss/ num_rows)
- #   print(changeProfLoss/ )
+    print(f'Total Months: {len(list1)} ')
+    print(f'Total: ${sum(list1)}')
+    print(f'Average Change:  ${(round(sum(list2)/len(list2),2))}') 
+    print(f'Greatest Increase in Profits: {The_month}  (${(max(list2))}) ')
+    print(f'Greatest Decrease in Profits: {The_month2}  (${(min(list2))}) ')
